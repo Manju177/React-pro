b23b6a84c9f39fb0db040bc72ec102164a055e05
Manju177 pattanashetti <manjunathpattanashetti166@gmail.com>
2023-11-25 19:47:51 -0800

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SiginUp from './Pages/SiginUp'
+import Profile from './Pages/Profile'
 
 function App() {
   return (
     <div>
-      <h1 className='text-red-800'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SiginUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
     
   )

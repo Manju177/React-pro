f954b36f3aac4e31f5f595e29b1817347541e4c7
Manju177 pattanashetti <manjunathpattanashetti166@gmail.com>
2024-01-12 21:31:41 -0800

    listing page completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './Pages/CreateListing'
 import UpdateListing from './Pages/UpdateListing'
+import Listing from './Pages/Listing'
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SiginUp />} />
+          <Route path='/listing/:listingId' element={<Listing/>} />
           <Route element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
             <Route path='/create-listing' element={<CreateListing />} />
